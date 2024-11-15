d7f009ecd271e1196c3f5d9f014f8501807bcd32
Siddhant Janbandhu <63910007+Siddhant-15@users.noreply.github.com>
2024-11-15 15:02:06 +0530

    Update server.js

--- car-management-app/server/server.js
+++ car-management-app/server/server.js
@@ -18,7 +18,13 @@ const connectDB = require('./config/db');
 const app = express();
 
 // Middleware
-app.use(cors()); // Enable Cross-Origin Resource Sharing
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-lwhq.vercel.app"],
+        methods: ["POST", "GET", "PUT", "DELETE"],
+        credentials: true
+    }
+)); // Enable Cross-Origin Resource Sharing
 app.use(express.json()); // Parse incoming JSON requests
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve images statically
 
